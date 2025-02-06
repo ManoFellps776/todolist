b20825a23829c41678a6f6451cad1ef34cfde5ed
Felipe Rezende <felipera776@gmail.com>
2025-02-05 21:13:47 -0300

    teste funcionando

--- src/test/java/br/com/projetospring/projeto_spring/ProjetoSpringApplicationTests.java
+++ src/test/java/br/com/projetospring/projeto_spring/ProjetoSpringApplicationTests.java
@@ -3,36 +3,35 @@ package br.com.projetospring.projeto_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.projetospring.projeto_spring.entity.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ProjetoSpringApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
 	@Test
-	void testCreatTodoSuccess() {
-		var todo = new Todo("todo 1","desc todo 1", false, 1);
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
 		webTestClient
-		.post()
-		.uri("/todos")
-		.bodyValue(todo)
-		.exchange()
-		.expectStatus().isOk()
-		.expectBody()
-		.jsonPath("$").isArray()
-		.jsonPath("$.length()").isEqualTo(1)
-		.jsonPath("$[0].nome()").isEqualTo(todo.getNome())
-		.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
-		.jsonPath("$[0].realizado()").isEqualTo(todo.isRealizado())
-		.jsonPath("$[0].prioridade()").isEqualTo(todo.getPrioridade());
-
-
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
 	}
 
 	@Test
-	void testCreatTodoFailure() {
+	void testCreateTodoFailure() {
 	}
 
 }
