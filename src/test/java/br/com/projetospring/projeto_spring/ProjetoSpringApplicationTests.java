43e3d4efb97c22a412282201d3bd67f3e0f132a7
Felipe Rezende <felipera776@gmail.com>
2025-02-07 14:45:55 -0300

    adicionado o failure

--- src/test/java/br/com/projetospring/projeto_spring/ProjetoSpringApplicationTests.java
+++ src/test/java/br/com/projetospring/projeto_spring/ProjetoSpringApplicationTests.java
@@ -32,6 +32,13 @@ class ProjetoSpringApplicationTests {
 
 	@Test
 	void testCreateTodoFailure() {
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(
+				new Todo("","",false,0)
+			).exchange()
+			.expectStatus().isBadRequest();
 	}
 
 }
