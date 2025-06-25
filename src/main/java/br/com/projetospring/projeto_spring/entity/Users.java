b275cd3ecbff84353cae8a93ddeb27af8924bee1
Felipe Rezende <felipera776@gmail.com>
2025-06-25 00:30:28 -0300

    Mudança novamente para Users.

--- src/main/java/br/com/projetospring/projeto_spring/entity/Users.java
+++ src/main/java/br/com/projetospring/projeto_spring/entity/Users.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "usuarios") // Evita conflito com "users"
+@Table(name = "users") // Evita conflito com "users"
 public class Users {
 
     @Id
