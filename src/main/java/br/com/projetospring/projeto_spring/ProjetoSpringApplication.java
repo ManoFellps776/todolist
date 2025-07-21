99b242df5f39ef4d51fc3647c64668ce55d521ca
Felipe Rezende <felipera776@gmail.com>
2025-07-21 19:18:25 -0300

    remoção do .env

--- src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
+++ src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
@@ -3,16 +3,12 @@ package br.com.projetospring.projeto_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ProjetoSpringApplication {
 
 	public static void main(String[] args) {
-		// Carrega .env
-		 Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-    SpringApplication.run(ProjetoSpringApplication.class, args);
+		
+		SpringApplication.run(ProjetoSpringApplication.class, args);
 	}
 
 }
