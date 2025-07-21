024fed374888ae6ee53eea945c29eec9bac09538
Felipe Rezende <felipera776@gmail.com>
2025-07-21 16:55:12 -0300

    Mudança no ProjetoSpringApp, no .env e no appproperties

--- src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
+++ src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
@@ -3,10 +3,15 @@ package br.com.projetospring.projeto_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ProjetoSpringApplication {
 
 	public static void main(String[] args) {
+		// Carrega .env
+		Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ProjetoSpringApplication.class, args);
 	}
 
