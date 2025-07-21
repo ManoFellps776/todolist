52d9cd5408821dfacc3b5c366b9888f0c85b79a5
Felipe Rezende <felipera776@gmail.com>
2025-07-21 19:45:54 -0300

    teste para compilar 1

--- src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
+++ src/main/java/br/com/projetospring/projeto_spring/ProjetoSpringApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetoSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("EMAIL = " + System.getenv("spring.mail.host"));
+System.out.println("EMAIL from props = " + System.getProperty("spring.mail.host"));
+
 		SpringApplication.run(ProjetoSpringApplication.class, args);
 	}
 
